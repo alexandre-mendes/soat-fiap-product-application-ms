b9854a4d7c086d3ac8a6aaa72aab19b0de7ff685
Alexandre Mendes da Costa <alexandremendes1005@gmail.com>
2025-07-23 18:45:19 -0300

    Ajustes

--- src/infra/api/productRouter.ts
+++ src/infra/api/productRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { productController } from '../config/di-config';
+import { wrapHandler } from './errorHandler';
 
 const productRouter = Router();
 
-productRouter.post('/api/products',                   productController.create.bind(productController));
-productRouter.put('/api/products/:id',                productController.update.bind(productController));
-productRouter.delete('/api/products/:id',             productController.delete.bind(productController));
-productRouter.get('/api/products/category/:category', productController.findAllByCategory.bind(productController));
+productRouter.post('/api/products',                   wrapHandler(productController.create.bind(productController)));
+productRouter.put('/api/products/:id',                wrapHandler(productController.update.bind(productController)));
+productRouter.delete('/api/products/:id',             wrapHandler(productController.delete.bind(productController)));
+productRouter.get('/api/products/category/:category', wrapHandler(productController.findAllByCategory.bind(productController)));
 
 export default productRouter;
